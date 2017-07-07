0674b5fc399dec587ae170141f861df3844c2f3f
fuegoel <lee.nylk@gmail.com>
2017-07-07 16:06:08 +0300

    Blah

--- .git-prompt-colors.sh
+++ .git-prompt-colors.sh
@@ -9,9 +9,9 @@ override_git_prompt_colors() {
   GIT_PROMPT_SEPARATOR=""              # separates each item
 
   GIT_PROMPT_BRANCH="${Magenta}"        # the git branch that is active in the current directory
-  GIT_PROMPT_STAGED="${Yellow}●"           # the number of staged files/directories
-  GIT_PROMPT_CONFLICTS="${Red}✖ "       # the number of files in conflict
-  GIT_PROMPT_CHANGED="${Blue}✚ "        # the number of changed files
+  GIT_PROMPT_STAGED="${Yellow} ● "           # the number of staged files/directories
+  GIT_PROMPT_CONFLICTS="${Red} ✖ "       # the number of files in conflict
+  GIT_PROMPT_CHANGED="${Blue} ✚ "        # the number of changed files
 
   # GIT_PROMPT_REMOTE=" "                 # the remote branch name (if any) and the symbols for ahead and behind
   GIT_PROMPT_UNTRACKED="${Cyan}…"       # the number of untracked files/dirs
